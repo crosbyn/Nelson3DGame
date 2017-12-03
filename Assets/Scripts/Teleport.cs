cd25e46459e410ebb20b501be9c4c10081c4136d
crosbyn <crn222crn@gmail.com>
2017-12-03 16:37:10 -0700

    Added new starting area and two rooms, and portals

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    
+    public Transform teleportReceive;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.transform.position = teleportReceive.position;
+        audioSource.Play();
+    }
+}
+
