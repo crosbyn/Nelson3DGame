3346625c6707daea2766d8477540a6eb69a518df
crosbyn <crn222crn@gmail.com>
2017-12-09 23:02:17 -0700

    Added labels to portals and created title and credits scenes

---
+++ Assets/Scripts/Title&CreditScreenScripts/TitleScreen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+
+    public void StartGameButtonClicked()
+    {
+        //Load the next scene
+        SceneManager.LoadScene("Scene1");
+    }
+
+    public void CreditsButtonClicked()
+    {
+        //Load the next scene
+        SceneManager.LoadScene("CreditsScene");
+    }
+
+    public void HowToPlayButtonClicked()
+    {
+        //Load the next scene
+        SceneManager.LoadScene("ControlsScene");
+    }
+
+    public void QuitButtonClicked()
+    {
+        Application.Quit();
+    }
+}
